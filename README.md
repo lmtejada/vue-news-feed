9c7298a502b8ff6341b3f32ff195ec3f67eabc04
Graham Sutton <grahamsutton2@gmail.com>
2019-06-24 09:26:42 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -10,11 +10,6 @@ npm install
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
 ### Run your tests
 ```
 npm run test
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">SproutLoud News!</h1>
+    <div class="well">
+      <!-- Add your news feed here -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
-<style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  background-color: #00955f;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-bottom: 60px;
+}
+
+.title {
+  color: #FFF;
+}
+
+.well {
+  max-width: 800px;
+  border-radius: 5px;
+  background-color: #FFF;
+  padding: 0 20px;
+}
+
+.button {
+  padding: 6px 8px;
+  border: 0;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:focus {
+    outline: 0;
+  }
+
+  &:hover {
+    background-color: #BBB;
+  }
+
+  &:active {
+    background-color: #AAA;
+  }
+
+  &.button-link {
+    background-color: transparent;
+    color: #009bb2;
+
+    &:hover {
+      cursor: pointer;
+      text-decoration: underline;
+    }
+  }
+}
+
+.input {
+  width: 100%;
+  padding: 6px 8px;
+  border-radius: 5px;
+  border: 1px solid #BBB;
+}
+
+.text-muted {
+  opacity: 0.75;
 }
 </style>
